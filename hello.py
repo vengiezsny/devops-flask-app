96d88288dabefee6de6096d68db392b91d75dbb4
vengiezsny <c20366171@mytudublin.ie>
2025-09-25 13:47:35 +0000

    added contact page with email

--- hello.py
+++ hello.py
@@ -2,7 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def say_hello():
-	return '<p>Hello, World,I am a Flask app!</p> <a href="/about">ABOUT PAGE</a>'
+	return '<p>Hello, World,I am a Flask app! This is the HOMEPAGE</p> <a href="/about">ABOUT PAGE</a> | <a href="/contact">Contact</a>'
 @app.route('/about')
 def about():
 	return '<p>Hello about</p>'
+@app.route('/contact')
+def contact():
+	return "Contact me at: c20366171@mytudublin.ie"
